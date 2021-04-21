8c9df67d678caa57058e5bb78376b4ad3800aed4
Samuel Ballan <sgb4622@gmail.com>
2021-04-21 19:38:57 -0400

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <textarea v-model="text" name="" cols="30" rows="10"></textarea>
+
+  {{text}}
+
+  <ul>
+    <li v-for="message in messages">
+      {{message}}
+    </li>
+
+  </ul>
+
+</template>
+
+<script lang="ts">
+import { ref, watch, defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Home',
+  setup: () => {
+    const text = ref<String>("")
+    const messages = ref<String[]>([])
+
+    watch(text, (newValue, oldValue) => {
+      messages.value = newValue.split("\n\n")
+    })
+    return { text, messages }
+  }
+})
+</script>
+
+<style scoped>
+</style>

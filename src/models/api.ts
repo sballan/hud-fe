d527c042ddaee6b4e700f07804ac32e6e4aa9391
Samuel Ballan <sgb4622@gmail.com>
2021-05-07 07:16:47 -0400

    cleaning

--- src/models/api.ts
+++ src/models/api.ts
@@ -1,13 +1,20 @@
 import { Note } from './note'
+import { Doc } from './doc'
 
-const mockNoteStore = new Map<number, Note>()
-mockNoteStore.set(1, new Note(1, "test parent note", []))
-mockNoteStore.set(2, new Note(2, "test child note", []))
+const note1 = new Note(1, "test parent note", [])
+const note2 = new Note(2, "test child note", [])
+const doc1 = new Doc(1, [note1, note2])
+
+const mockNoteApi = new Map<number, Note>()
+const mockDocApi = new Map<number, Doc>()
+mockDocApi.set(1, doc1)
+mockNoteApi.set(1, note1)
+mockNoteApi.set(2, note2)
 
 export const api = {
   notes: {
     async get(id: number): Promise<Note> {
-      const mockNote = mockNoteStore.get(id)
+      const mockNote = mockNoteApi.get(id)
       return new Promise((resolve, reject) => {
         if (mockNote) resolve(mockNote)
         else reject()
--- src/models/note.spec.ts
+++ src/models/note.spec.ts
@@ -1,13 +1,8 @@
 import { Note } from './note'
 
 describe('Note', () => {
-  it('can be instantiated without a parent', () => {
+  it('can be instantiated', () => {
     const note = new Note(1, "test", [])
-    expect(note).toBeTruthy()
-  })
-
-  it('can be instantiated with a parent', () => {
-    const note = new Note(2, "test", [])
     expect(note).toBeTruthy()
   })
 })

f803a5ab4eb589429aadb4276e1629ae3416a10b
Samuel Ballan <sgb4622@gmail.com>
2021-05-07 07:55:02 -0400

    testing out the doc view

---
+++ src/components/Doc.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Doc {{ doc.id }}
+
+    <ul>
+      <li v-for="note in doc.children">
+        {{note.content}}
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script lang="ts">
+import { ref, watch, defineComponent, PropType } from 'vue'
+import { Doc } from '../models/doc'
+export default defineComponent({
+  name: 'Doc',
+  props: {
+    doc: {
+      type: Doc as PropType<Doc>
+    }
+  }
+})
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,15 +5,19 @@
     <li v-for="message in messages">
       {{message}}
     </li>
-
   </ul>
+
+  <Doc :doc="{id: 1, children: [{id: 1, content: 'test'}]}"></Doc>
 
 </template>
 
 <script lang="ts">
 import { ref, watch, defineComponent } from 'vue'
+import Doc from './Doc.vue'
+
 export default defineComponent({
   name: 'Home',
+  components: { Doc },
   setup: () => {
     const text = ref<String>("")
     const messages = ref<String[]>([])

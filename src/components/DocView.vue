3602b6d0aa0d1eb0cc0ca78e7eac30db8db7e004
Samuel Ballan <sgb4622@gmail.com>
2021-05-08 07:58:17 -0400

    disambiguate doc and docview. start setting up a docview with nested notes

--- src/components/DocView.vue
+++ src/components/DocView.vue
@@ -3,7 +3,7 @@
     Doc {{ doc.id }}
 
     <ul>
-      <li v-for="note in doc.children">
+      <li v-for="note in doc.notes">
         {{note.content}}
       </li>
     </ul>
@@ -12,13 +12,15 @@
 </template>
 
 <script lang="ts">
-import { ref, watch, defineComponent, PropType } from 'vue'
+import { defineComponent, PropType } from 'vue'
 import { Doc } from '../models/doc'
+
 export default defineComponent({
-  name: 'Doc',
+  name: 'DocView',
   props: {
     doc: {
-      type: Doc as PropType<Doc>
+      type: Doc as PropType<Doc>,
+      required: true
     }
   }
 })
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,25 +7,33 @@
     </li>
   </ul>
 
-  <Doc :doc="{id: 1, children: [{id: 1, content: 'test'}]}"></Doc>
+  <DocView v-if="doc" :doc="doc"></DocView>
 
 </template>
 
 <script lang="ts">
-import { ref, watch, defineComponent } from 'vue'
-import Doc from './Doc.vue'
+import { ref, watch, defineComponent, onBeforeMount } from 'vue'
+import DocView from './DocView.vue'
+import { Doc } from '../models/doc'
+import { api } from '../models/api'
 
 export default defineComponent({
   name: 'Home',
-  components: { Doc },
+  components: { DocView },
   setup: () => {
     const text = ref<String>("")
     const messages = ref<String[]>([])
-
     watch(text, (newValue, oldValue) => {
       messages.value = newValue.split("\n\n")
     })
-    return { text, messages }
+
+    const docRef = ref<Doc>()
+    onBeforeMount(async () => {
+      const doc = await api.docs.get(1)
+      docRef.value = doc
+    })
+
+    return { text, messages, doc: docRef }
   }
 })
 </script>
